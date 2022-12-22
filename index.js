7e7529ab11359ebf96c7ce6a5b1832ee0f322c7a
Godwin-NJ <amadigodwin7@gmail.com>
2022-12-22 02:58:04 +0100

    security packages added

--- index.js
+++ index.js
@@ -6,23 +6,48 @@ const app = express();
 const connectDB = require("./connect");
 const morgan = require("morgan");
 const cors = require("cors");
+// security
+const mongoSanitize = require("express-mongo-sanitize");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+// routes
 const inventoryRoute = require("./Routes/inventoryRoute");
 const vendorRoute = require("./Routes/VendorRoute");
 const purchaseRoute = require("./Routes/PurchaseRoute");
 const customerRoute = require("./Routes/customerRoute");
 const salesRoute = require("./Routes/salesRoute");
 const authRouter = require("./Routes/userAuthRoute");
+// error handling
 const NotFoundMiddleware = require("./Middleware/notFound");
 const errorHandler = require("./Middleware/error-handlerMiddleware");
 
+// Swagger
+// const swaggerUi = require("swagger-ui-express");
+// const YAML = require("yamljs");
+// const swaggerDocument = YAML.load("./swagger.yaml");
 // middleware
+app.set("trust proxy", 1);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
+app.use(mongoSanitize());
+app.use(limiter);
+app.use(helmet());
+app.use(xss());
 
+//this confirms that our API is up
 app.get("/", (req, res) => {
-  res.send("unit inventory");
+  res.send("<h1>Api Docs</h1>");
 });
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/inventory", inventoryRoute);
@@ -35,7 +60,7 @@ app.use("/api/v1/user", authRouter);
 app.use(NotFoundMiddleware);
 app.use(errorHandler);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const connectApp = async () => {
   const url = process.env.MONGO_DB;
   await connectDB(url);
